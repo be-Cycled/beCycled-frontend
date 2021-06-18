389a20e401726187dc18b653882ceae8cbf116ed
Sam Bulatov <mephistorine@gmail.com>
2021-06-18 21:41:50 +0300

    feat: Реализована страница регистрации (#1)

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
-import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core'
+import { TuiButtonModule, TuiErrorModule, TuiLinkModule } from '@taiga-ui/core'
 import { TUI_VALIDATION_ERRORS, TuiFieldErrorModule, TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/kit'
 
 import { AuthRoutingModule } from './auth-routing.module'
@@ -10,7 +10,6 @@ import { LoginComponent } from './components/login/login.component'
 import { RegistrationComponent } from './components/registration/registration.component'
 import { AuthContainerComponent } from './containers/auth-container/auth-container.component'
 import { AuthorizationService } from './services/authorization/authorization.service'
-
 
 @NgModule({
   declarations: [
@@ -27,7 +26,8 @@ import { AuthorizationService } from './services/authorization/authorization.ser
     TuiInputPasswordModule,
     ReactiveFormsModule,
     TuiButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    TuiErrorModule
   ],
   providers: [
     AuthorizationService,
