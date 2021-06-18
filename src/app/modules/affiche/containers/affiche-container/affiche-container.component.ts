b9d87fbbdfea4b38375f1756fe71f43760f18a2b
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-06-18 22:02:34 +0300

    feat: Интерфейсы для компонента афиши (#2)

--- src/app/modules/affiche/containers/affiche-container/affiche-container.component.ts
+++ src/app/modules/affiche/containers/affiche-container/affiche-container.component.ts
@@ -1,9 +1,28 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { ISO8601 } from '../../../../global/models'
+import { Competition, Workout } from '../../../../global/domain'
+
+enum EventType {
+  workout = 'WORKOUT',
+  competition = 'COMPETITION'
+}
+
+interface WrappedEvent<T, U> {
+  type: T,
+  value: U
+}
+
+interface EventListByDay {
+  date: ISO8601
+  events: SomeWrappedEvent[]
+}
+
+type SomeWrappedEvent = WrappedEvent<EventType.workout, Workout> | WrappedEvent<EventType.competition, Competition>
 
 @Component({
   selector: 'cy-affiche-container',
   templateUrl: './affiche-container.component.html',
-  styleUrls: ['./affiche-container.component.scss'],
+  styleUrls: [ './affiche-container.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AfficheContainerComponent implements OnInit {
