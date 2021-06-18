cd0ea09018e971f5a5f06ee4673a8a2f39d80a19
Sam Bulatov <mephistorine@gmail.com>
2021-06-18 17:46:27 +0300

    feat: Указаны основные роуты

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AfficheModule } from './modules/affiche/affiche.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { CommunitiesModule } from './modules/communities/communities.module'
+import { FeedModule } from './modules/feed/feed.module'
+import { PostsModule } from './modules/posts/posts.module'
+import { ProfileModule } from './modules/profile/profile.module'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/affiche/affiche.module').then((module: { AfficheModule: AfficheModule }) => module.AfficheModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then((module: { AuthModule: AuthModule }) => module.AuthModule)
+  },
+  {
+    path: 'feed',
+    loadChildren: () => import('./modules/feed/feed.module').then((module: { FeedModule: FeedModule }) => module.FeedModule)
+  },
+  {
+    path: 'communities',
+    loadChildren: () => import('./modules/communities/communities.module').then((module: { CommunitiesModule: CommunitiesModule }) => module.CommunitiesModule)
+  },
+  {
+    path: 'posts',
+    loadChildren: () => import('./modules/posts/posts.module').then((module: { PostsModule: PostsModule }) => module.PostsModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./modules/profile/profile.module').then((module: { ProfileModule: ProfileModule }) => module.ProfileModule)
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
