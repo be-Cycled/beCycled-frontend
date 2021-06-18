1c27c0e3c38aaf46dc027787c36e68f28f5f4187
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-06-18 19:42:44 +0300

    feat: Карта mapbox и обёртка для Angular (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { TuiRootModule } from '@taiga-ui/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { AppComponent } from './app.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    TuiRootModule
+    TuiRootModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoiYXZrb2x0b3ZpY2giLCJhIjoiY2twazRzamx2M2hoODJvbnhjZzB6eHJlayJ9.e5IMZ_ELx1EPzucgUlIH8g',
+      geocoderAccessToken: 'pk.eyJ1IjoiYXZrb2x0b3ZpY2giLCJhIjoiY2twazRzamx2M2hoODJvbnhjZzB6eHJlayJ9.e5IMZ_ELx1EPzucgUlIH8g'
+    })
   ],
   providers: [],
   bootstrap: [ AppComponent ]
