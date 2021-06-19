5d53d1cb0fc778e95d41c5d425bd4b5d1025c4a3
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-06-19 22:19:11 +0300

    feat: Теперь есть возможность попасть попасть на детальную информацию соревнования (#6)

--- src/app/modules/competition-page/competition-page-routing.module.ts
+++ src/app/modules/competition-page/competition-page-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { CompetitionPageComponent } from './components/competition-page/competition-page.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: ':id',
+    component: CompetitionPageComponent
+  }
+]
 
 @NgModule({
   imports: [ RouterModule.forChild(routes) ],
--- src/app/modules/competition-page/competition-page.module.ts
+++ src/app/modules/competition-page/competition-page.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { CompetitionPageRoutingModule } from './competition-page-routing.module'
+import { CompetitionPageComponent } from './components/competition-page/competition-page.component'
+import { TuiIslandModule } from '@taiga-ui/kit'
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl'
+import { TuiLetModule } from '@taiga-ui/cdk'
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CompetitionPageComponent
+  ],
   imports: [
     CommonModule,
-    CompetitionPageRoutingModule
+    CompetitionPageRoutingModule,
+    TuiIslandModule,
+    NgxMapboxGLModule,
+    TuiLetModule
   ]
 })
 export class CompetitionPageModule {
