f8a97ca69951d75bc6f261824eb553b7f6728ba0
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-06-19 21:36:25 +0300

    feat: Теперь можно провалиться в тренировку по клику (#6)

---
+++ src/app/modules/competition-page/competition-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { CompetitionPageRoutingModule } from './competition-page-routing.module'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CompetitionPageRoutingModule
+  ]
+})
+export class CompetitionPageModule {
+}
