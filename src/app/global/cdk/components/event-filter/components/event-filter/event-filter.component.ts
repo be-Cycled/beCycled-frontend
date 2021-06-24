6f2ddad568557331dd7c1f47577893d6f4c82ce6
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-06-24 18:59:41 +0300

    fix: Теперь в тегах фильтра актуальная информация счетчика по видам спорта

--- src/app/global/cdk/components/event-filter/components/event-filter/event-filter.component.ts
+++ src/app/global/cdk/components/event-filter/components/event-filter/event-filter.component.ts
@@ -1,16 +1,19 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   forwardRef,
   Input,
   OnChanges,
+  OnDestroy,
+  OnInit,
   SimpleChanges
 } from '@angular/core'
 import { TuiHandler, TuiIdentityMatcher } from '@taiga-ui/cdk'
 import { EventType } from '../../../../../models'
 import { Competition, SportType, Workout } from '../../../../../domain'
 import { ControlValueAccessor, FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms'
+import { takeUntil, tap } from 'rxjs/operators'
+import { Observable, Subject } from 'rxjs'
 
 interface FilterTag {
   title: string
@@ -31,7 +34,9 @@ interface FilterTag {
     }
   ]
 })
-export class EventFilterComponent implements ControlValueAccessor, OnChanges {
+export class EventFilterComponent implements ControlValueAccessor, OnChanges, OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject()
+
   public items: FilterTag[] = [
     {
       title: 'Тренировки',
@@ -69,20 +74,66 @@ export class EventFilterComponent implements ControlValueAccessor, OnChanges {
     filters: new FormControl()
   })
 
+  /**
+   * Поток, который следит за изменением выбранных фильтров и актуализирует счетчик в тегах
+   */
+  private badgeSportTypeCountChanges$: Observable<{ filters: FilterTag[] }> = this.form.valueChanges.pipe(
+    takeUntil(this.destroy$),
+    tap(({ filters }: { filters: FilterTag[] }) => {
+      const selectedValues: string[] = filters.map((item: FilterTag) => item.value)
+
+      if (selectedValues.includes(EventType.workout) && !selectedValues.includes(EventType.competition)) {
+        this.resetSportTypeBadgeCount()
+
+        this.updateSportTypeBadgesCount(this.events[ 0 ])
+      } else if (selectedValues.includes(EventType.competition) && !selectedValues.includes(EventType.workout)) {
+        this.resetSportTypeBadgeCount()
+
+        this.updateSportTypeBadgesCount(this.events[ 1 ])
+      } else {
+        this.resetSportTypeBadgeCount()
+
+        this.updateAllBadgesCount(this.events)
+      }
+    })
+  )
+
+  /**
+   * Ивенты нужны только для актуализации данных счетчиков в тегах
+   */
   @Input()
   public events: [ Workout[], Competition[] ] = [ [], [] ]
 
-  constructor(private cd: ChangeDetectorRef) {
+  public ngOnInit(): void {
+    this.badgeSportTypeCountChanges$.subscribe()
   }
 
-  public identityMatcher: TuiIdentityMatcher<FilterTag> = (
-    item1: FilterTag,
-    item2: FilterTag
-  ) => item1.title === item2.title
+  public ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
 
-  public badgeHandler: TuiHandler<Record<string, any>, number> = (item: Record<string, any>) => item.count
+  public resetSportTypeBadgeCount(): void {
+    const badges: string[] = Object.values(SportType)
 
-  public setBadgesCount([ workouts, competitions ]: [ Workout[], Competition[] ]): void {
+    this.items.forEach((item: FilterTag) => {
+      if (badges.includes(item.value)) {
+        item.count = 0
+      }
+    })
+  }
+
+  public updateSportTypeBadgesCount(events: Workout[] | Competition[]): void {
+    events.forEach((event: Workout | Competition) => {
+      const currentBadge: FilterTag | undefined = this.items.find((filterTag: FilterTag) => event.sportType === filterTag.value)
+
+      if (typeof currentBadge !== 'undefined') {
+        currentBadge.count += 1
+      }
+    })
+  }
+
+  public updateAllBadgesCount([ workouts, competitions ]: [ Workout[], Competition[] ]): void {
     this.items.find((filterTag: FilterTag) => filterTag.value === EventType.workout)!.count = workouts.length
     this.items.find((filterTag: FilterTag) => filterTag.value === EventType.competition)!.count = competitions.length;
 
@@ -110,9 +161,16 @@ export class EventFilterComponent implements ControlValueAccessor, OnChanges {
 
   public ngOnChanges({ events }: SimpleChanges): void {
     if (events.currentValue !== null) {
-      this.setBadgesCount(events.currentValue)
+      this.updateAllBadgesCount(events.currentValue)
 
       this.form.get('filters')?.reset([])
     }
   }
+
+  public identityMatcher: TuiIdentityMatcher<FilterTag> = (
+    item1: FilterTag,
+    item2: FilterTag
+  ) => item1.title === item2.title
+
+  public badgeHandler: TuiHandler<Record<string, any>, number> = (item: Record<string, any>) => item.count
 }
