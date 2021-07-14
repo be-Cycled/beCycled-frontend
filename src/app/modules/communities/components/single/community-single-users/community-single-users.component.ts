ecec2027736b2f252a3b7e784da74ca2f03079e2
Sam Bulatov <mephistorine@gmail.com>
2021-07-14 23:39:34 +0300

    feat: Добавлен вывод участников (#32)

--- src/app/modules/communities/components/single/community-single-users/community-single-users.component.ts
+++ src/app/modules/communities/components/single/community-single-users/community-single-users.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { Observable } from 'rxjs'
-import { switchMap } from 'rxjs/operators'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { BehaviorSubject, defer, Observable, of } from 'rxjs'
+import { catchError, shareReplay, switchMap, take, tap } from 'rxjs/operators'
 import { Community, User } from '../../../../../global/domain'
 import { CommunityService } from '../../../../../global/domain/services/community/community.service'
 import { CommunityStoreService } from '../../../services'
@@ -11,16 +11,30 @@ import { CommunityStoreService } from '../../../services'
   styleUrls: ['./community-single-users.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CommunitySingleUsersComponent {
+export class CommunitySingleUsersComponent implements OnInit {
 
-  public users: Observable<User[]> = this.communityStoreService.communityChanges.pipe(
-    switchMap((community: Community) => this.communityService.getUsersByCommunity(community.nickname).pipe(
+  public userShowLoader: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
 
-    ))
-  )
+  public users: Observable<User[]> = defer(() => this.communityStoreService.communityChanges.pipe(
+    switchMap((community: Community) => {
+      this.userShowLoader.next(true)
+
+      return this.communityService.getUsersByCommunity(community.nickname).pipe(
+        tap(() => this.userShowLoader.next(false)),
+        catchError(() => of([]))
+      )
+    }),
+    shareReplay({ refCount: true, bufferSize: 1 })
+  ))
+
+  public community: Observable<Community> = this.communityStoreService.communityChanges.pipe()
 
   constructor(private communityStoreService: CommunityStoreService,
               private communityService: CommunityService) {
+  }
+
+  public ngOnInit(): void {
+    this.users.pipe(take(1)).subscribe()
   }
 
 }
