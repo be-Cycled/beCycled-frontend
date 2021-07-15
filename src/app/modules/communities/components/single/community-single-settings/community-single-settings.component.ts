752fd17397956bd4b829f8c9cf04d3f174f500e6
Sam Bulatov <mephistorine@gmail.com>
2021-07-15 22:36:02 +0300

    feat: Начата работа над секцией настроек сообщества (#32)

--- src/app/modules/communities/components/single/community-single-settings/community-single-settings.component.ts
+++ src/app/modules/communities/components/single/community-single-settings/community-single-settings.component.ts
@@ -1,16 +1,30 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { CommunityStoreService } from '../../../services'
 
 @Component({
   selector: 'cy-community-single-settings',
   templateUrl: './community-single-settings.component.html',
-  styleUrls: ['./community-single-settings.component.scss'],
+  styleUrls: [ './community-single-settings.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommunitySingleSettingsComponent implements OnInit {
 
-  constructor() { }
+  public communityFormGroup: FormGroup = this.fb.group({
+    name: this.fb.control(null),
+    nickname: this.fb.control(null),
+    avatar: this.fb.control(null),
+    sportTypes: this.fb.control([]),
+    url: this.fb.control(null),
+    description: this.fb.control(null),
+    communityType: this.fb.control(null)
+  })
 
-  ngOnInit(): void {
+  constructor(private communityStoreService: CommunityStoreService,
+              private fb: FormBuilder) {
+  }
+
+  public ngOnInit(): void {
   }
 
 }
