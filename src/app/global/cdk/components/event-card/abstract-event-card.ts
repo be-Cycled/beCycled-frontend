5671e6e140ee489c906f3c80502e5a53059579fc
Aleksandr Koltovich <koltovich.a@firstmk.ru>
2021-07-15 23:17:06 +0300

    feat: Теперь метод генерирования строки длительности принимает аргумент в секундах (#68)

--- src/app/global/cdk/components/event-card/abstract-event-card.ts
+++ src/app/global/cdk/components/event-card/abstract-event-card.ts
@@ -39,16 +39,21 @@ export abstract class AbstractEventCard {
     return `${ distanceInKilometres } км`
   }
 
+  /**
+   * Принимает секунды в качестве аргумента
+   */
   public generateDurationString(duration: number): string {
+    const durationInMinutes: number = duration / 60
+
     let hours: number = 0
     let minutes: number = 0
 
-    if (duration < 60) {
-      return `${ buildCountString(duration, [ 'минута', 'минуты', 'минут' ]) }`
+    if (durationInMinutes < 60) {
+      return `${ buildCountString(durationInMinutes, [ 'минута', 'минуты', 'минут' ]) }`
     }
 
-    hours = Math.floor(duration / 60)
-    minutes = duration - (hours * 60)
+    hours = Math.floor(durationInMinutes / 60)
+    minutes = durationInMinutes - (hours * 60)
 
     return `${ buildCountString(hours, [ 'час', 'часа', 'часов' ]) }
      ${ minutes === 0
