37c651694227083578ba3a4424440802425b2db5
Sam Bulatov <mephistorine@gmail.com>
2021-07-30 11:09:29 +0300

    feat: Добавлена секция "О команде" (#118)
    - Добавлена ссылка на Фейсбук и на твиттер
    - Добавлены варианты логотипа с PNG

--- src/app/modules/about/containers/about-container/about-container.component.ts
+++ src/app/modules/about/containers/about-container/about-container.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutContainerComponent {
-
+  public year: number = new Date().getFullYear()
 }
